f1219a8e597ad64702cc4adc00be1132b3a7f5e2
Benjamin Manns <benmanns@gmail.com>
2010-08-19 09:43:13 -0400

    Created CookieClient class.

---
+++ OgreLogin/CookieClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace OgreIsland.Login
+{
+    public class CookieClient : WebClient
+    {
+        public CookieContainer Cookies { get; private set; }
+        public CookieClient() { Cookies = new CookieContainer(); }
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest) (request as HttpWebRequest).CookieContainer = Cookies;
+            return request;
+        }
+    }
+}
