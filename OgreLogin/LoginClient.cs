4569ca046525ad57175833bc35a812b86595828b
Benjamin Manns <benmanns@gmail.com>
2010-08-19 10:09:43 -0400

    Added LoadCharacters method to LoginClient.

--- OgreLogin/LoginClient.cs
+++ OgreLogin/LoginClient.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Text.RegularExpressions;
 
 namespace OgreIsland.Login
 {
     public class LoginClient : CookieClient
     {
+        private static readonly Regex CharacterRegex = new Regex("<a id=\\\"page_content_charlist_ctl([0-9]{2,})_charname\\\" href=\\\"javascript:__doPostBack\\('page\\$content\\$charlist\\$ctl([0-9]{2,})\\$charname',''\\)\\\">([a-zA-Z][a-zA-Z0-9 ]{2,19})</a>", RegexOptions.Compiled);
+
         private readonly Uri _homeUri = new Uri("home.aspx", UriKind.Relative);
+        private readonly Uri _gameUri = new Uri("game.aspx", UriKind.Relative);
+
+        public Dictionary<string, string> Characters { get; private set; }
 
         public LoginClient(string baseAddress) { BaseAddress = baseAddress; }
 
@@ -26,6 +33,14 @@ namespace OgreIsland.Login
                                                 {"page$btnLogin", "Login"}
                                             };
             UploadValues(_homeUri, "POST", login);
+        }
+
+        public void LoadCharacters()
+        {
+            string game = DownloadString(_gameUri);
+            MatchCollection characterMatches = CharacterRegex.Matches(game);
+            Characters = new Dictionary<string, string>(characterMatches.Count);
+            foreach (Match character in characterMatches) Characters.Add(character.Groups[2].Value, character.Groups[3].Value);
         }
     }
 }
