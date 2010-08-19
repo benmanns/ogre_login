698f17bb4465047ea6de1e716f1f14355810a44d
Benjamin Manns <benmanns@gmail.com>
2010-08-19 09:43:47 -0400

    Created Tokens class.

---
+++ OgreLogin/Tokens.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OgreIsland.Login
+{
+    public class Tokens
+    {
+        private static readonly Regex ViewStateRegex = new Regex("<input type=\\\"hidden\\\" name=\\\"__VIEWSTATE\\\" id=\\\"__VIEWSTATE\\\" value=\\\"([0-9a-zA-Z\\+/=]{20,})\\\" />", RegexOptions.Compiled);
+        private static readonly Regex EventValidationRegex = new Regex("<input type=\\\"hidden\\\" name=\\\"__EVENTVALIDATION\\\" id=\\\"__EVENTVALIDATION\\\" value=\\\"([0-9a-zA-Z\\+/=]{20,})\\\" />", RegexOptions.Compiled);
+
+        public string ViewState { get; private set; }
+        public string EventValidation { get; private set; }
+
+        public Tokens(string document)
+        {
+            ViewState = ViewStateRegex.Match(document).Groups[1].Value;
+            EventValidation = EventValidationRegex.Match(document).Groups[1].Value;
+        }
+    }
+}
